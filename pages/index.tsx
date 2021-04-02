9a7dd8cab5cb8f3c3ac9b0b5c3dd58f466304927
Bryan Pan <bryanpan342@gmail.com>
2021-04-01 22:38:25 -0700

    feat: starting the windows desktop

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
-import React from 'react';
+import Image from 'next/image';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import Draggable from 'react-draggable';
 import styles from '../styles/Home.module.scss';
 import { contentfulQuery } from '../utils';
 
@@ -15,20 +17,50 @@ interface HomeProps {
 
 export default function Home(props: HomeProps): JSX.Element {
   const {links} = props;
+  const [dragging, setDragging] = useState(false);
+  const timeout = useRef(null);
+
+  useEffect(() => {
+    return () => timeout.current && clearTimeout(timeout.current);
+  }, []);
+
+  const onDrag = useCallback(() => {
+    console.log('dragging');
+    setDragging(true);
+    clearTimeout(timeout.current);
+    timeout.current = setTimeout(() => setDragging(false), 250);
+  }, [dragging]);
+
+  const handleClick = (e) => {
+    if (!dragging) return;
+    e.preventDefault();
+    e.stopPropagation();
+  }
+  
   return (
     <div className={styles.container}>
       <Head>
         <title>Bryan's Links</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
-        <ul>
-          {links.map(({url, displayName}) => 
-            <a href={url} rel='noreferrer' target='_blank'><li>{displayName}</li></a>
-          )}
-        </ul>
+        {links.map(({url, displayName}) =>
+          <Draggable
+            onDrag={onDrag}>
+              <div onClick={handleClick}>
+                <a href={url} target='_blank' rel='noreferrer'>
+                  <div>
+                    {displayName}
+                  </div>
+                </a>
+              </div>
+          </Draggable>
+        )}
+      <div className={styles.taskbar}>
+        <button className={styles.startButton}>
+          <Image src={'/windows.png'} width='16' height='16'/>
+          <div id={styles.text}>Start</div>
+        </button>
       </div>
-
     </div>
   );
 }
